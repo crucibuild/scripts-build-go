cdeb0a4abe26a6e0f9246e4dbbd1aef1b7e03678
Jonathan Pigrée <jonathan.pigree@gmail.com>
2018-02-01 13:06:10 +0100

    Remove --update flag from gometalinter call
    Since linters are now vendored with the tool itself.

--- script/check.sh
+++ script/check.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH="$( readlink -f "${BASH_SOURCE[0]}" )"
 SCRIPT_DIR="$( dirname "${SCRIPT_PATH}" )"
 
 go get -u github.com/alecthomas/gometalinter
-gometalinter --install --update
+gometalinter --install
 gometalinter -j2 --config "${SCRIPT_DIR}/../conf/gometalinter.json" ./...
